d27c12033b10d1bc96a123b1b5e8430ca8497c4c
feiyu <lvp123321123321@gmail.com>
2025-03-27 00:32:00 +0700

    support working without db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,20 +1,28 @@
 import { Db, MongoClient } from "mongodb";
 
 namespace ZeroDb {
-  const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
-  const client = new MongoClient(url);
+  const url = process.env.MONGODB_URI as string;
 
   // Database Name
   const dbName = "zerosecure";
   let db: Db;
 
   export async function connect() {
+    if (!url) {
+      return console.log(
+        "WARNING: MONGODB_URI is not set, all database operations will throw errors"
+      );
+    }
+    const client = new MongoClient(url);
     await client.connect();
     db = client.db(dbName);
     console.log("Connected successfully to mongodb server " + url);
   }
 
   export function getDb() {
+    if (!db) {
+      throw new Error("Must connect to database first");
+    }
     return db;
   }
 
